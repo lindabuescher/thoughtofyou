734e74f6c74ffbe7f0c0bd83c8e47f7e8ea43619
linda <lindabuescher18@gmail.com>
2026-02-24 14:45:59 +0100

    Responsive Design #1

--- web-projects/first-website/js/main.js
+++ web-projects/first-website/js/main.js
@@ -1,6 +1,5 @@
-// Store a reference to the <h1> in a variable
+//old
 const myHeading = document.querySelector("h1");
-// Update the text content of the <h1>
 myHeading.textContent = "Happy Galentines, Leyla!";
 
 
@@ -15,22 +14,22 @@ myImage.addEventListener("click", () => {
   }
 });
 
-// Define the correct password
-const correctPassword = "yourPassword123";
 
-// Get the input field
+const correctPassword = "1234";
+
+
 const passwordInput = document.getElementById("password");
 
-// Add an event listener to the input field
+
 passwordInput.addEventListener("keydown", (event) => {
-    // Check if the pressed key is "Enter"
+    
     if (event.key === "Enter") {
-        const enteredPassword = passwordInput.value.trim(); // Get the entered password
+        const enteredPassword = passwordInput.value.trim(); 
         if (enteredPassword === correctPassword) {
-            // Redirect to the access page
+            
             window.location.href = "access.html";
         } else {
-            // Show an error message
+            
             alert("Incorrect password. Please try again.");
         }
     }
